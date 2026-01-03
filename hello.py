6d7234f36832885fb91d9b2e9d81c2cc7e85a127
MILAD AT <58528910+milad15A@users.noreply.github.com>
2026-01-03 07:06:43 -0800

    python-CI/CD 2

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
+"""
+This is a simple Python script that prints 'Hello, World!' to the console.
+It serves as a demonstration of a basic Python program.
+"""
 
+print("Hello, World!")
 print("hello, world")
